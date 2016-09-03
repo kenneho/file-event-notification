574bdda3830c235fab7d209aeb96365c26d1c6d8
Kenneth Holter <kenneth.holter@protonmail.com>
2016-09-03 08:28:52 +0200

    Replaced custom file tail code with the tailer module

--- watchfile.py
+++ watchfile.py
@@ -5,9 +5,8 @@ import httplib, urllib
 import re
 import sys, getopt
 import syslog
+import tailer
 
-filename = '/var/log/ruter.log'
-regexp_pattern = 'Dropping frame due to full tx queue|error|warning|rebooting|shutdown'
 user_token = ''
 app_token = ''
 
@@ -16,22 +15,9 @@ def analyze_line(line):
     if re.search(regexp_pattern, line, re.IGNORECASE):
         send_notification(line)
 
-def tail_file(filename):
-    file = open(filename,'r')
-    
-    #Find the size of the file and move to the end
-    st_results = os.stat(filename)
-    st_size = st_results[6]
-    file.seek(st_size)
-
-    while 1:
-        where = file.tell()
-        line = file.readline()
-        if not line:
-            time.sleep(1)
-            file.seek(where)
-        else:
-            analyze_line(line)
+def tail_file(filename): 
+    for line in tailer.follow(open(filename)): 
+        analyze_line(line)
 
 def send_notification(line):
     syslog.syslog("This line triggered a notification: " + line)
@@ -45,25 +31,26 @@ def send_notification(line):
         "html": "0",                                # 1 for HTML, 0 to disable
         "title": "Rasberry Pi notification",                # Title of the message
         "message": line,     # Content of the message
-        #"url": "http://IP.ADD.RE.SS",               # Link to be included in message
-        #"url_title": "View live stream",            # Text for the link
-        #"sound": "siren",                           # Define the sound played
         }), { "Content-type": "application/x-www-form-urlencoded" })
     response = conn.getresponse()
 
 def main(argv):
    global app_token
    global user_token
+   global regexp_pattern
+
    try:
-      opts, args = getopt.getopt(argv,"a:u:",["app-token=","user-token="])
+      opts, args = getopt.getopt(argv,"a:u:r:",["app-token=","user-token=","regexp="])
    except getopt.GetoptError:
-      print 'Parameters: -a <apptoken> -u <usertoken>'
+      print 'Parameters: -a <apptoken> -u <usertoken> -r <regexp>'
       sys.exit(2)
    for opt, arg in opts:
       if opt in ("-a", "--app-token"):
          app_token = arg
       elif opt in ("-u", "--user-token"):
          user_token = arg
+      elif opt in ("-r", "--regexp"):
+         regexp_pattern = arg
 
    tail_file("/var/log/ruter.log")
 
